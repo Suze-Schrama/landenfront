7b9e2a4e349b13a730fd9fb90aef9f18beb6c518
Suze Schrama <suze.schrama@gmail.com>
2024-09-18 13:28:51 +0200

    re added the unversioned files and now it works. Why???

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {WelcomeComponent} from "./welcome/welcome.component";
+import {LandenComponent} from "./landen/landen.component";
 
 @Component({
   selector: 'app-root',
-  template:'<app-welcome></app-welcome>',
+  template:'<app-welcome></app-welcome><app-landen></app-landen>',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent],
+  imports: [RouterOutlet, WelcomeComponent, LandenComponent],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
